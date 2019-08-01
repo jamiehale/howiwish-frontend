aa2e9630a612a23956cf2f303047e06969715194
Jamie Hale <jamiesonhale@gmail.com>
2019-07-31 22:38:29 -0400

    Add my list page

--- src/pages/my-list/MyListPage.js
+++ src/pages/my-list/MyListPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import List from '../../components/List';
+import ListItem from '../../components/ListItem';
 
-const MyListPage = () => (
-  <h1>My List</h1>
-);
+const myListItems = [
+  'A thing',
+  'Another thing',
+  'A third thing',
+];
+
+const MyListPage = () => {
+  const items = myListItems.map((item, i) => (
+    <ListItem key={i}>{item}</ListItem>
+  ));
+
+  return (
+    <>
+      <h1>My List</h1>
+      <List>
+        {items}
+      </List>
+    </>
+  );
+};
 
 export default MyListPage;
